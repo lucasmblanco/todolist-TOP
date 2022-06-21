299b408e6fe3479ed4c0884e7ea8947c65bb7fd4
Lucas <lucasmatiasblanco@outlook.com>
2022-06-21 16:24:04 -0300

    2nd fix logo

--- src/script/header/header.js
+++ src/script/header/header.js
@@ -1,15 +1,20 @@
+import logo from '../../components/main-page/todo-logo.png'
+
 const headerComponents = (function(){
 
-
-    const header = document.querySelector('header'); 
-    const imgElement = header.querySelector('img'); 
+    const logoContainer = document.querySelector('div#logo-container'); 
+    
 
     const addLogo = function(){
+        const imgElement = document.createElement('img'); 
+        imgElement.setAttribute('alt', 'page-logo')
+        imgElement.src = logo
         imgElement.classList.add('header-image'); 
+        logoContainer.append(imgElement); 
     }
     
 
-    return {addLogo}
+    return { addLogo }
 })();
 
 export default headerComponents
